57f29c307dd9a0bbe5e6b35873e91e3349a3133a
Brandon Franks <bgfranks106@icloud.com>
2019-10-07 22:51:59 -0400

    finished this questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Global Object binding is when this is used in the window object and returns everything in the window
+ * 2. Implict Binding is when a function is called with a dot. What is before the dot is this.
+ * 3. New biding is when an instance of the object is created and returned by the constructor.
+ * 4. Explicit binding is when JS methods are used to explictly define this.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
